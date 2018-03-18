9e4cd6fbdc1b9d13cbc3de83008cfc0693b87eeb
ileler <ileler@qq.com>
2018-03-18 18:49:16 +0800

    suppport lock model

--- parser/UnlockedSensorParser.js
+++ parser/UnlockedSensorParser.js
@@ -7,13 +7,17 @@ class UnlockedSensorParser extends DeviceParser {
     }
 
     getAccessoriesParserInfo() {
+        var parserInfo = {
+            'UnlockedSensor_UnlockedSensor': UnlockedSensorUnlockedSensorParser
+        };
         //get Config
         var accessoryConfig = this.platform.ConfigUtil.getAccessoryConfig(this.deviceSid);
-        console.log(accessoryConfig);
-        return {
-            'UnlockedSensor_UnlockedSensor': UnlockedSensorUnlockedSensorParser,
-            '65536': VirtualBaseMotionParser
+        if (accessoryConfig) {
+            for (var key in accessoryConfig) {
+                parserInfo[key] = VirtualBaseMotionParser;
+            }
         }
+        return parserInfo;
     }
 }
 
@@ -59,8 +63,9 @@ class UnlockedSensorUnlockedSensorParser extends AccessoryParser {
         var that = this;
         var deviceSid = jsonObj['sid'];
         var uuid = that.getAccessoryUUID(deviceSid);
+        var value = that.getValueFrJsonObjData(jsonObj, 'unlocked');
         var accessory = that.platform.AccessoryUtil.getByUUID(uuid);
-        if(accessory) {
+        if(!value && accessory) {
             var service = accessory.getService(that.Service.MotionSensor);
             var motionDetectedCharacteristic = service.getCharacteristic(that.Characteristic.MotionDetected);
             var value = that.getMotionDetectedCharacteristicValue(jsonObj, null);
@@ -68,34 +73,21 @@ class UnlockedSensorUnlockedSensorParser extends AccessoryParser {
                 motionDetectedCharacteristic.updateValue(value);
             }
 
-            if(that.platform.ConfigUtil.getAccessorySyncValue(deviceSid, that.accessoryType)) {
-                if (motionDetectedCharacteristic.listeners('get').length == 0) {
-                    motionDetectedCharacteristic.on("get", function(callback) {
-                        var command = '{"cmd":"read", "sid":"' + deviceSid + '"}';
-                        that.platform.sendReadCommand(deviceSid, command).then(result => {
-                            var value = that.getMotionDetectedCharacteristicValue(result, null);
-                            if(null != value) {
-                                callback(null, value);
-                            } else {
-                                callback(new Error('get value fail: ' + result));
-                            }
-                        }).catch(function(err) {
-                            that.platform.log.error(err);
-                            callback(err);
-                        });
-                    });
-                }
-            }
-
             that.parserBatteryService(accessory, jsonObj);
         }
     }
 
     getMotionDetectedCharacteristicValue(jsonObj, defaultValue) {
-        var newObj = JSON.parse("{\"cmd\":\"report\",\"model\":\"sensor_motion\",\"sid\":\"65536\",\"data\":\"{\\\"status\\\":\\\"motion\\\"}\"}");
-        this.platform.ParseUtil.parserAccessories(newObj);
-        var value = this.getValueFrJsonObjData(jsonObj, 'status');
-        return (null != value) ? (value === 'motion') : false;
+        var value = this.getValueFrJsonObjData(jsonObj, 'fing_verified') || this.getValueFrJsonObjData(jsonObj, 'card_verified') || this.getValueFrJsonObjData(jsonObj, 'psw_verified');
+        this.platform.ParseUtil.parserAccessories({
+            "cmd": "report",
+            "model": this.model,
+            "sid": jsonObj['sid'],
+            "data": JSON.stringify({
+                "unlocked": "true",
+                "user_id": value
+            })});
+        return (null != value) ? this.getValueFrJsonObjData(jsonObj, 'verified_wrong') : false;
     }
 }
 
@@ -143,7 +135,7 @@ class VirtualBaseMotionParser extends AccessoryParser {
     }
 
     getMotionDetectedCharacteristicValue(jsonObj, defaultValue) {
-        var value = this.getValueFrJsonObjData(jsonObj, 'status');
-        return (null != value) ? (value === 'motion') : false;
+        var value = this.getValueFrJsonObjData(jsonObj, 'user_id');
+        return (null != value) ? (value === this.accessoryType) : false;
     }
 }
