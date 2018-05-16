6e7f1e87bf3918e4cf18d7007e90e2b92199403f
Seyd <34152761+SeydX@users.noreply.github.com>
2018-05-16 14:04:12 +0200

    Add fakegato

--- parser/TemperatureAndHumiditySensor2Parser.js
+++ parser/TemperatureAndHumiditySensor2Parser.js
@@ -1,5 +1,8 @@
 const DeviceParser = require('./DeviceParser');
 const AccessoryParser = require('./AccessoryParser');
+const moment = require('moment');
+
+var FakeGatoHistoryService;
 
 class TemperatureAndHumiditySensor2Parser extends DeviceParser {
     constructor(model, platform) {
@@ -22,6 +25,8 @@ module.exports = TemperatureAndHumiditySensor2Parser;
 class TemperatureAndHumiditySensor2TemperatureSensorParser extends AccessoryParser {
     constructor(model, platform, accessoryType) {
         super(model, platform, accessoryType)
+        FakeGatoHistoryService = require('fakegato-history')(this.platform.api)
+        this.HBpath = this.platform.api.user.storagePath()+'/accessories';
     }
     
     getAccessoryCategory(deviceSid) {
@@ -90,12 +95,36 @@ class TemperatureAndHumiditySensor2TemperatureSensorParser extends AccessoryPars
             }
             
             that.parserBatteryService(accessory, jsonObj);
+            that.getTempHistory(jsonObj, null);
         }
     }
     
     getCurrentTemperatureCharacteristicValue(jsonObj, defaultValue) {
         var value = this.getValueFrJsonObjData(jsonObj, 'temperature');
         return (null != value) ? (value / 100.0) : defaultValue;
+    }
+    
+    getTempHistory(jsonObj, defaultValue){
+	    var that = this;
+        var deviceSid = jsonObj['sid'];
+        var uuid = that.getAccessoryUUID(deviceSid);
+	    var accessory = that.platform.AccessoryUtil.getByUUID(uuid);
+	    if(accessory){
+		  accessory.context.loggingService = new FakeGatoHistoryService("weather",accessory,{storage:'fs',path:that.HBpath, disableTimer: true});
+		  accessory.context.loggingService.log = that.platform.log.log;
+  	      var value = that.getCurrentTemperatureCharacteristicValue(jsonObj, null);
+	      if(null != value){
+            accessory.context.loggingService.addEntry({
+              time: moment().unix(),
+              temp: value,
+              pressure: 0,
+              humidity: 0
+            });
+	      }
+	    }
+	    setTimeout(function(){
+		    that.getTempHistory(jsonObj, defaultValue);
+	    }, 8 * 60 * 1000) //8min
     }
 }
 
@@ -166,12 +195,36 @@ class TemperatureAndHumiditySensor2HumiditySensorParser extends AccessoryParser 
             }
             
             that.parserBatteryService(accessory, jsonObj);
+            that.getHumidityHistory(jsonObj, null);
         }
     }
     
     getCurrentRelativeHumidityCharacteristicValue(jsonObj, defaultValue) {
         var value = this.getValueFrJsonObjData(jsonObj, 'humidity');
         return (null != value) ? (value / 100.0) : defaultValue;
+    }
+    
+    getHumidityHistory(jsonObj, defaultValue){
+	    var that = this;
+        var deviceSid = jsonObj['sid'];
+        var uuid = that.getAccessoryUUID(deviceSid);
+	    var accessory = that.platform.AccessoryUtil.getByUUID(uuid);
+	    if(accessory){
+		  accessory.context.loggingService = new FakeGatoHistoryService("weather",accessory,{storage:'fs',path:that.HBpath, disableTimer: true});
+		  accessory.context.loggingService.log = that.platform.log.log;
+  	      var value = that.getCurrentRelativeHumidityCharacteristicValue(jsonObj, null);
+	      if(null != value){
+            accessory.context.loggingService.addEntry({
+              time: moment().unix(),
+              temp: 0,
+              pressure: 0,
+              humidity: value
+            });
+	      }
+	    }
+	    setTimeout(function(){
+		    that.getHumidityHistory(jsonObj, defaultValue);
+	    }, 8 * 60 * 1000) //8min
     }
 }
 /*
